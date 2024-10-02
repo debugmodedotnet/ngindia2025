93d7dd3722174399b739008851e88ee0564b61a4
Ariba <ariba.madakia@gmail.com>
2024-10-02 16:16:08 +0530

    Major changes

---
+++ src/app/components/coc/coc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coc',
+  standalone: true,
+  imports: [],
+  templateUrl: './coc.component.html',
+  styleUrl: './coc.component.scss'
+})
+export class CocComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,12 +4,14 @@ import { SpeakersComponent } from "../speakers/speakers.component";
 import { AgendaComponent } from "../agenda/agenda.component";
 import { VenueComponent } from "../venue/venue.component";
 import { PricingComponent } from "../pricing/pricing.component";
-import { SocialComponent } from "../social/social.component";
+import { ContactComponent } from "../contact/contact.component";
+import { SponsorsComponent } from "../sponsors/sponsors.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, SpeakersComponent, AgendaComponent, VenueComponent, PricingComponent, SocialComponent],
+  imports: [HeroComponent, SpeakersComponent, AgendaComponent, VenueComponent, PricingComponent, ContactComponent, SponsorsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
