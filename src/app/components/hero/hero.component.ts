d52cb7a928c1542100ad0be40771ebda8eade45b
Ariba <ariba.madakia@gmail.com>
2024-10-03 16:16:52 +0530

    Hero changes

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
+import { TagSliderComponent } from '../tag-slider/tag-slider.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [TagSliderComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+
+  // count: WritableSignal<number> = signal(10);
+  // isEven: Signal<boolean> = computed(() => {
+  //   return this.count() % 2 === 0;
+  // });
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log('Count is even', this.isEven());
+  //   })
+  // }
 
 }
