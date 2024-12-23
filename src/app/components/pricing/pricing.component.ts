a27809753f653776c0ba96f67932afa5b29b5276
Ariba <ariba.madakia@gmail.com>
2024-12-23 16:06:04 +0530

    Minor changes

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,13 +1,14 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
 export class PricingComponent {
 
-  isDisabled = true; 
+  isDisabled = true;
 }
