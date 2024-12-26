dc7f156fc22484730bc97c988ded706ca434af93
Ariba <ariba.madakia@gmail.com>
2024-12-26 21:28:59 +0530

    Minor changes

--- src/app/dto/album.ts
+++ src/app/dto/album.ts
@@ -9,33 +9,70 @@ export const albums: IAlbum[] = [
     image: "assets/images/albums/2023.jpg"
   },
   {
-    link: "",
+    link: "https://photos.app.goo.gl/aJvCsPgSeLoGsMxKA",
     year: "2023",
     title: "ng-India 2023 Photo Album",
     image: "assets/images/albums/2023.jpg"
   },
   {
-    link: "",
+    link: "https://photos.app.goo.gl/QMyQnCqXU27uAh4G6",
     year: "2022",
     title: "ng-India 2022 Photo Album",
     image: "assets/images/albums/2023.jpg"
   },
   {
-    link: "",
-    year: "2021",
-    title: "ng-India 2021 Photo Album",
-    image: "assets/images/albums/2023.jpg"
+    link: "https://photos.google.com/share/AF1QipMOs5NDzr_sJ_QrvT82xUj_N0mKReOO5wbO3dGrjlI-1gTVSikDgpvf_4T6btT0fw?key=cHBPVC1iLWRUdHJ3MnVRMzBjb3FwdFEyN3dmR0tR",
+    year: "2020",
+    title: "ng-India 2020 Photo Album",
+    image: "assets/images/albums/2020.jpg"
   },
   {
-    link: "",
+    link: "https://photos.app.goo.gl/5ghyyo8tCLaLCDb29",
     year: "2019",
     title: "ng-India 2019 Photo Album",
-    image: "assets/images/albums/2023.jpg"
+    image: "assets/images/albums/2019.jpg"
   },
   {
-    link: "",
+    link: "https://photos.app.goo.gl/L4sRxBkJeMpgJcoB3",
     year: "2018",
     title: "ng-India 2018 Photo Album",
-    image: "assets/images/albums/2023.jpg"
+    image: "assets/images/albums/2018.jpg"
   }
 ];
+//   {
+//     link: "",
+//     year: "2024",
+//     title: "ng-India 2024 Photo Album",
+//     image: "assets/images/albums/2023.jpg"
+//   },
+//   {
+//     link: "https://photos.app.goo.gl/aJvCsPgSeLoGsMxKA",
+//     year: "2023",
+//     title: "ng-India 2023 Photo Album",
+//     image: "assets/images/albums/2023.jpg"
+//   },
+//   {
+//     link: "https://photos.app.goo.gl/QMyQnCqXU27uAh4G6",
+//     year: "2022",
+//     title: "ng-India 2022 Photo Album",
+//     image: "assets/images/albums/2023.jpg"
+//   },
+//   {
+//     link: "https://photos.google.com/share/AF1QipMOs5NDzr_sJ_QrvT82xUj_N0mKReOO5wbO3dGrjlI-1gTVSikDgpvf_4T6btT0fw?key=cHBPVC1iLWRUdHJ3MnVRMzBjb3FwdFEyN3dmR0tR",
+//     year: "2020",
+//     title: "ng-India 2020 Photo Album",
+//     image: "assets/images/albums/2020.jpg"
+//   },
+//   {
+//     link: "https://photos.app.goo.gl/5ghyyo8tCLaLCDb29",
+//     year: "2019",
+//     title: "ng-India 2019 Photo Album",
+//     image: "assets/images/albums/2019.jpg"
+//   },
+//   {
+//     link: "https://photos.app.goo.gl/L4sRxBkJeMpgJcoB3",
+//     year: "2018",
+//     title: "ng-India 2018 Photo Album",
+//     image: "assets/images/albums/2018.jpg"
+//   }
+// ];
