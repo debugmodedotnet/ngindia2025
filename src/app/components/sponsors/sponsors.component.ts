63ded2b3fdca26642a8b6c87a67e82f048065efa
Ariba <ariba.madakia@gmail.com>
2024-12-26 13:45:51 +0530

    Minor changes

--- src/app/components/sponsors/sponsors.component.ts
+++ src/app/components/sponsors/sponsors.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ISponsors } from '../../models/sponsors';
 import { sponsors } from '../../dto/sponsors.json';
 import { NgClass } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sponsors',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, RouterLink],
   templateUrl: './sponsors.component.html',
   styleUrl: './sponsors.component.scss'
 })
