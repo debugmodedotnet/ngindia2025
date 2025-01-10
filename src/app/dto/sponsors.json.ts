c33acde5315c8b333b80560e79aeb0aea89117c4
dkumar-mbai <dkumar@matchbookai.com>
2025-01-10 19:57:58 +0530

    sb

--- src/app/dto/sponsors.json.ts
+++ src/app/dto/sponsors.json.ts
@@ -1,24 +1,29 @@
 import { ISponsors } from "../models/sponsors";
 
 export const sponsors: ISponsors[] = [
-    {
-        sponsorLink: "https://code-pushup.dev/",
-        sponsorImage: "assets/images/sponsors/code-pushup.jpeg",
-        sponsorType: "Bronze"
-    },
-    {
-        sponsorLink: "https://a4technology.com/",
-        sponsorImage: "assets/images/sponsors/a4tech.jpeg",
-        sponsorType: "Bronze"
-    },
-    {
-        sponsorLink: "https://rezoomex.com/",
-        sponsorImage: "assets/images/sponsors/rezoomex.png",
-        sponsorType: "Bronze"
-    },
-    {
-        sponsorLink: "https://matchbookai.com/",
-        sponsorImage: "assets/images/sponsors/matchbook-ai.jpeg",
-        sponsorType: "Bronze"
-    }
+  {
+    sponsorLink: 'https://code-pushup.dev/',
+    sponsorImage: 'assets/images/sponsors/code-pushup.jpeg',
+    sponsorType: 'Bronze',
+  },
+  {
+    sponsorLink: 'https://a4technology.com/',
+    sponsorImage: 'assets/images/sponsors/a4tech.jpeg',
+    sponsorType: 'Bronze',
+  },
+  {
+    sponsorLink: 'https://squareboat.com/',
+    sponsorImage: 'assets/images/sponsors/squareboat.png',
+    sponsorType: 'Bronze',
+  },
+  {
+    sponsorLink: 'https://rezoomex.com/',
+    sponsorImage: 'assets/images/sponsors/rezoomex.png',
+    sponsorType: 'Bronze',
+  },
+  {
+    sponsorLink: 'https://matchbookai.com/',
+    sponsorImage: 'assets/images/sponsors/matchbook-ai.jpeg',
+    sponsorType: 'Bronze',
+  },
 ];
