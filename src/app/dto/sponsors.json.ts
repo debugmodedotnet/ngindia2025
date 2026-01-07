c57acfafa8823165a55e9e34b9964d50a6f05d48
unknown <nidhishnaghera3@gmail.com>
2026-01-07 16:15:23 +0530

    Added sponsor

--- src/app/dto/sponsors.json.ts
+++ src/app/dto/sponsors.json.ts
@@ -1,59 +1,64 @@
 import { ISponsors } from "../models/sponsors";
 
 export const sponsors: ISponsors[] = [
+  // {
+  //   sponsorLink: 'https://ivy.global/',
+  //   sponsorImage: 'assets/images/sponsors/ivy.png',
+  //   sponsorType: 'Silver',
+  // },
+  // {
+  //   sponsorLink: 'https://search.app/DQ9sioxPDQbpPeVd7',
+  //   sponsorImage: 'assets/images/sponsors/scholarhat.png',
+  //   sponsorType: 'Silver',
+  // },
+  // {
+  //   sponsorLink: 'https://www.7n.com/',
+  //   sponsorImage: 'assets/images/sponsors/7n.png',
+  //   sponsorType: 'Silver',
+  // },
+  // {
+  //   sponsorLink: 'https://rezoomex.com/',
+  //   sponsorImage: 'assets/images/sponsors/rezoomex.png',
+  //   sponsorType: 'Silver',
+  // },
+  // {
+  //   sponsorLink: 'https://nx.dev/',
+  //   sponsorImage: 'assets/images/sponsors/nx.png',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://a4technology.com/',
+  //   sponsorImage: 'assets/images/sponsors/a4tech.jpeg',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://www.telerik.com/kendo-angular-ui',
+  //   sponsorImage: 'assets/images/sponsors/kendo.png',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://squareboat.com/',
+  //   sponsorImage: 'assets/images/sponsors/squareboat.png',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://code-pushup.dev/',
+  //   sponsorImage: 'assets/images/sponsors/code-pushup.jpeg',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://matchbookai.com/',
+  //   sponsorImage: 'assets/images/sponsors/matchbook-ai.jpeg',
+  //   sponsorType: 'Bronze',
+  // },
+  // {
+  //   sponsorLink: 'https://hrone.cloud/',
+  //   sponsorImage: 'assets/images/sponsors/hrone.png',
+  //   sponsorType: 'Bronze',
+  // }
   {
-    sponsorLink: 'https://ivy.global/',
-    sponsorImage: 'assets/images/sponsors/ivy.png',
-    sponsorType: 'Silver',
-  },
-  {
-    sponsorLink: 'https://search.app/DQ9sioxPDQbpPeVd7',
-    sponsorImage: 'assets/images/sponsors/scholarhat.png',
-    sponsorType: 'Silver',
-  },
-  {
-    sponsorLink: 'https://www.7n.com/',
-    sponsorImage: 'assets/images/sponsors/7n.png',
-    sponsorType: 'Silver',
-  },
-  {
-    sponsorLink: 'https://rezoomex.com/',
-    sponsorImage: 'assets/images/sponsors/rezoomex.png',
-    sponsorType: 'Silver',
-  },
-  {
-    sponsorLink: 'https://nx.dev/',
-    sponsorImage: 'assets/images/sponsors/nx.png',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://a4technology.com/',
-    sponsorImage: 'assets/images/sponsors/a4tech.jpeg',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://www.telerik.com/kendo-angular-ui',
-    sponsorImage: 'assets/images/sponsors/kendo.png',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://squareboat.com/',
-    sponsorImage: 'assets/images/sponsors/squareboat.png',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://code-pushup.dev/',
-    sponsorImage: 'assets/images/sponsors/code-pushup.jpeg',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://matchbookai.com/',
-    sponsorImage: 'assets/images/sponsors/matchbook-ai.jpeg',
-    sponsorType: 'Bronze',
-  },
-  {
-    sponsorLink: 'https://hrone.cloud/',
-    sponsorImage: 'assets/images/sponsors/hrone.png',
-    sponsorType: 'Bronze',
+    sponsorLink: 'http://www.questpond.com',
+    sponsorImage: 'assets/images/sponsors/quest-pond.jpeg',
+    sponsorType: ''
   }
 ];
